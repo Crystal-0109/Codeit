5254c6328233d89979f106f594eeee760a088f41
soojung moon <parkfam044@naver.com>
2024-02-07 20:10:54 +0900

    DELETE request 보내기

--- WebAPI/PUTDELETErequest.js
+++ WebAPI/PUTDELETErequest.js
@@ -4,15 +4,9 @@
 //   .then((response) => response.text())
 //   .then((result) => { console.log(result); });
 
-const member = {
-  name: 'Alice',
-  email: 'alice@codeitmall.kr',
-  department: 'marketing',
-};
 
 fetch('https://learn.codeit.kr/api/members/2', {
-  method: 'PUT',
-  body: JSON.stringify(member),
+  method: 'DELETE',
 })
   .then((response) => response.text())
   .then((result) => { console.log(result); });
