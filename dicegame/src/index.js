bf2494aeb864ad7c0ee6d67767528100cb81e569
soojung moon <parkfam044@naver.com>
2024-02-11 16:42:16 +0900

    리액트 컴포넌트 사용 예제

--- dicegame/src/index.js
+++ dicegame/src/index.js
@@ -2,20 +2,19 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const product = "MacBook";
-const model = " Air";
-const item = product + model;
-const imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/MacBook_with_Retina_Display.png/500px-MacBook_with_Retina_Display.png";
-
-function handleClick() {
-  alert("곧 도착합니다!");
+function Hello() {
+  return <h1>안녕 리액트</h1>;
 }
 
-root.render(
+const element = (
   <>
-    <h1>나만의 {item} 주문하기</h1>
-    <img src={imageUrl} alt='제품 사진' />
-    <button onClick={handleClick}>확인</button>
+    <Hello />
+    <Hello />
+    <Hello />    
   </>
+)
+
+root.render(
+  element
   
 );
