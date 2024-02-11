ee6ce01a2cbcf50bb0c39d5da64fdae75600cc11
soojung moon <parkfam044@naver.com>
2024-02-11 19:49:08 +0900

    기록 state 추가

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -8,18 +8,22 @@ function random(n) {
 }
 
 function App() {
-  const [num, setNum] = useState(1);  // 초깃값
-  const [sum, setSum] = useState(0);
+  const [num, setNum] = useState(1);  // 주사위 초깃값
+  const [sum, setSum] = useState(0);  // 총점 초깃값
+  const [gameHistory, setGameHistory] = useState([]);  // 기록 초깃값 배열로 생성
 
   const handleRollClick = () => { // 던지기 버튼
     const nextNum = random(6);
     setNum(nextNum);
     setSum(sum + nextNum);
+    gameHistory.push(nextNum);
+    setGameHistory(gameHistory);
   }
 
   const handleClearClick = () => { // 처음부터 버튼(초기화 버튼)
     setNum(1);
     setSum(0);
+    setGameHistory([]);
   } 
 
   return (
@@ -33,6 +37,8 @@ function App() {
       <Dice color="blue" num={num} />
       <h2>총점</h2>
       <p>{sum}</p>
+      <h2>기록</h2>
+      <p>{gameHistory.join(', ')}</p>
     </div>
   </div>
   );
