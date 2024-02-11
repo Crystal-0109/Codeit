1f55104d013e3175f673d58122910c8dfe813151
soojung moon <parkfam044@naver.com>
2024-02-11 22:55:46 +0900

    주사위게임 디자인 수정

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Board from "./Board";
 import Button from "./Button";
 import './App.css';
+import logo from './assets/logo.png';
 
 function random(n) {
   return Math.ceil(Math.random() * n);
@@ -24,12 +25,15 @@ function App() {
   };
 
   return (
+    
     <div className="App">
+      <img className="App-logo" src={logo} alt="주사위 게임 로고" />
+      <h1 className="App-title">주사위게임</h1>
       <Button className="App-button" color="blue" onClick={handleRollClick}>던지기</Button>
       <Button className="App-button" color="red" onClick={handleClearClick}>처음부터</Button>
-      <div>
-        <Board name="나" color="blue" gameHistory={myHistory} />
-        <Board name="상대" color="red" gameHistory={otherHistory} />
+      <div className="App-boards">
+        <Board className="Board-heading" name="나" color="blue" gameHistory={myHistory} />
+        <Board className="Board-heading" name="상대" color="red" gameHistory={otherHistory} />
       </div>
     </div>
   );
