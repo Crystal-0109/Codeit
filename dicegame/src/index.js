9da642ba4c55ae3d9b015d620a21f02e46343ec2
soojung moon <parkfam044@naver.com>
2024-02-11 16:10:18 +0900

    속성에도 중괄호로 js 코드 적용 가능

--- dicegame/src/index.js
+++ dicegame/src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const product = "MacBook";
+const model = " Air";
+const item = product + model;
+const imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/MacBook_with_Retina_Display.png/500px-MacBook_with_Retina_Display.png";
 
 root.render(
-  <h1>나만의 {product.toUpperCase()} 주문하기</h1>
+  <>
+    <h1>나만의 {item} 주문하기</h1>
+    <img src={imageUrl} alt='제품 사진' />
+  </>
   
 );
