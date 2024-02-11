4523bc59a89ca7f2f34439184afed0438950eac3
soojung moon <parkfam044@naver.com>
2024-02-12 00:32:14 +0900

    안 쓰는 import문 수정

--- rock-scissor-paper/src/App.js
+++ rock-scissor-paper/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Button from './Button';
+// import Button from './Button';
 import HandButton from './HandButton';
 import HandIcon from './HandIcon';
 import { compareHand, generateRandomHand } from './utils';
