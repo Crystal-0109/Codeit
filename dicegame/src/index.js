99e7a7ae0a9e012771bd5ff820d06486ada1f234
soojung moon <parkfam044@naver.com>
2024-02-11 15:54:56 +0900

    Fragment 태그

--- dicegame/src/index.js
+++ dicegame/src/index.js
@@ -1,6 +1,11 @@
+import { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>안녕 리액트!</h1>
+  <Fragment>
+    <h1>안녕 리액트!</h1>
+    <h1>안녕 리액트!</h1>
+  </Fragment>
+  
 );
