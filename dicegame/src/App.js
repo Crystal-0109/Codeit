01c0225c37570d774950a41084c271cf1a4239c9
soojung moon <parkfam044@naver.com>
2024-02-11 19:26:11 +0900

    던지기, 처음부터 onClick 적용

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -2,18 +2,28 @@ import { useState } from "react";
 import Button from "./Button";
 import Dice from "./Dice";
 
-function App() {
-  const [num, setNum] = useState(1);
 
-  const handleRollClick = () => {
-    setNum(3);
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
+
+function App() {
+  const [num, setNum] = useState(1);  // 초깃값
+
+  const handleRollClick = () => { // 던지기 버튼
+    const nextNum = random(6);
+    setNum(nextNum);
   }
+
+  const handleClearClick = () => { // 처음부터 버튼(초기화 버튼)
+    setNum(1);
+  } 
 
   return (
   <div>
     <div>
       <Button onClick={handleRollClick}>던지기</Button>
-      <Button>처음부터</Button>
+      <Button onClick={handleClearClick}>처음부터</Button>
     </div>
     <Dice color="red" num={num} />
   </div>
