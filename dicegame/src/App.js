244fca6e145f8c471836a26ce24a55fafd4a4cfb
soojung moon <parkfam044@naver.com>
2024-02-11 20:21:57 +0900

    State Lifting

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -1,10 +1,43 @@
+import { useState } from "react";
 import Board from "./Board";
+import Button from "./Button";
+
+
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 function App() {
+  const [num, setNum] = useState(1);  // 주사위 초깃값
+  const [sum, setSum] = useState(0);  // 총점 초깃값
+  const [gameHistory, setGameHistory] = useState([]);  // 기록 초깃값 배열로 생성
+  const [otherNum, setOtherNum] = useState(1);  // 주사위 초깃값
+  const [otherSum, setOtherSum] = useState(0);  // 총점 초깃값
+  const [otherGameHistory, setOtherGameHistory] = useState([]);  // 기록 초깃값 배열로 생성
+
+  const handleRollClick = () => { // 던지기 버튼
+    const nextNum = random(6);
+    setNum(nextNum);
+    setSum(sum + nextNum);
+    setGameHistory([...gameHistory, nextNum]);
+  }
+
+  const handleClearClick = () => { // 처음부터 버튼(초기화 버튼)
+    setNum(1);
+    setSum(0);
+    setGameHistory([]);
+  } 
+
   return (
     <div>
-      <Board name="나" color="blue" />
-      <Board name="상대" color="red" />
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
+      </div>
+      <div>
+        <Board name="나" color="blue" />
+        <Board name="상대" color="red" />
+      </div>
     </div>
   );
 }
