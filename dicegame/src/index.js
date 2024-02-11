fe540dda654975d51d1b5fa5c2441dc658815649
soojung moon <parkfam044@naver.com>
2024-02-11 16:13:53 +0900

    이벤트 핸들러 속성에도 중괄호 속에서 js코드 사용 가능

--- dicegame/src/index.js
+++ dicegame/src/index.js
@@ -7,10 +7,15 @@ const model = " Air";
 const item = product + model;
 const imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/MacBook_with_Retina_Display.png/500px-MacBook_with_Retina_Display.png";
 
+function handleClick() {
+  alert("곧 도착합니다!");
+}
+
 root.render(
   <>
     <h1>나만의 {item} 주문하기</h1>
     <img src={imageUrl} alt='제품 사진' />
+    <button onClick={handleClick}>확인</button>
   </>
   
 );
