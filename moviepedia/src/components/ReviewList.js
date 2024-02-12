b236f786167a05bfeabd0b3389ec35edcddfcaf3
soojung moon <parkfam044@naver.com>
2024-02-12 22:30:05 +0900

    영화 목록 li태그로 출력

--- moviepedia/src/components/ReviewList.js
+++ moviepedia/src/components/ReviewList.js
@@ -1,6 +1,11 @@
 function ReviewList({ items }) {
-  console.log(items);
-  return <ul></ul>;
+  return (
+    <ul>
+      {items.map((item) => {
+        return <li>{item.title}</li>;
+      })}
+    </ul>
+  );
 }
 
 export default ReviewList;
