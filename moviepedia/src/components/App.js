21cf6bda8e8f6c7714b1f595bfcd362e0fe0a962
soojung moon <parkfam044@naver.com>
2024-02-12 22:50:54 +0900

    order가 createdAt(최신순)으로 정렬

--- moviepedia/src/components/App.js
+++ moviepedia/src/components/App.js
@@ -1,8 +1,10 @@
 import ReviewList from "./ReviewList";
 import items from "../mock.json";
+import { useState } from "react";
 
 function App() {
-  const sortedItems = items.sort((a, b) => b.rating - a.rating);
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   return (
     <div>
