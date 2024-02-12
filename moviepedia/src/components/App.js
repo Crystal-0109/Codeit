7652bb1bab4681dad12d98321655d352d77591ca
soojung moon <parkfam044@naver.com>
2024-02-13 01:02:02 +0900

    mock.json말고 네트워크에서 데이터 받아오기

--- moviepedia/src/components/App.js
+++ moviepedia/src/components/App.js
@@ -1,9 +1,9 @@
+import { getReviews } from "../api";
 import ReviewList from "./ReviewList";
-import mockItems from "../mock.json";
 import { useState } from "react";
 
 function App() {
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -16,6 +16,11 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews);
+  };
+
   return (
     <div>
       <div>
@@ -23,6 +28,7 @@ function App() {
         <button onClick={handleBestClick}>평점순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
