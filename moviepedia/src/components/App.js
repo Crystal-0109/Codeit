839da8d89989ee45f82b228b1e0ba503777b0144
soojung moon <parkfam044@naver.com>
2024-02-12 23:03:07 +0900

    최신순, 평점순 버튼 추가

--- moviepedia/src/components/App.js
+++ moviepedia/src/components/App.js
@@ -6,8 +6,16 @@ function App() {
   const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
+  const handleNewestClick = () => setOrder("createdAt");
+
+  const handleBestClick = () => setOrder("rating");
+
   return (
     <div>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>평점순</button>
+      </div>
       <ReviewList items={sortedItems} />
     </div>
   );
