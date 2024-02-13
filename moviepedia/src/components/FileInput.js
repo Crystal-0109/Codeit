4b6a20961a3f1e0a9e0b5b8017d1b676c92280e7
soojung moon <parkfam044@naver.com>
2024-02-13 19:44:58 +0900

    FileInput 완료

--- moviepedia/src/components/FileInput.js
+++ moviepedia/src/components/FileInput.js
@@ -1,11 +1,7 @@
-import { useState } from "react";
-
-function FileInput() {
-  const [value, setValue] = useState();
-
+function FileInput({ name, value, onChange }) {
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
-    setValue(nextValue);
+    onChange(name, nextValue);
   };
 
   return <input type="file" onChange={handleChange} />;
--- moviepedia/src/components/ReviewForm.js
+++ moviepedia/src/components/ReviewForm.js
@@ -3,15 +3,20 @@ import "./ReviewForm.css";
 import FileInput from "./FileInput";
 
 function ReviewForm() {
-  const [values, setValues] = useState({ title: "", rating: 0, content: "" });
+  const [values, setValues] = useState({ title: "", rating: 0, content: "", imgFile: null });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const handleChange = (name, value) => {
     setValues((prevValues) => ({
       ...prevValues,
       [name]: value,
     }));
   };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(values);
@@ -19,10 +24,10 @@ function ReviewForm() {
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
-      <FileInput />
-      <input name="title" value={values.title} onChange={handleChange} />
-      <input type="number" name="rating" value={values.rating} onChange={handleChange} />
-      <textarea name="content" value={values.content} onChange={handleChange} />
+      <FileInput name="imgFile" value={values.imgFile} onChange={handleChange} />
+      <input name="title" value={values.title} onChange={handleInputChange} />
+      <input type="number" name="rating" value={values.rating} onChange={handleInputChange} />
+      <textarea name="content" value={values.content} onChange={handleInputChange} />
       <button type="submit">확인</button>
     </form>
   );
