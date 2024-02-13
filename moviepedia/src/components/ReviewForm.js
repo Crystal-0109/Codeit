e389e617044b1ad31ebf9b619c2aa1881bbe8520
soojung moon <parkfam044@naver.com>
2024-02-13 21:55:50 +0900

    글 수정시 파일 미리보기 보이게

--- moviepedia/src/components/ReviewForm.js
+++ moviepedia/src/components/ReviewForm.js
@@ -6,7 +6,7 @@ import { createReview } from "../api";
 
 const INITIAL_VALUES = { title: "", rating: 0, content: "", imgFile: null };
 
-function ReviewForm({ initialValues = INITIAL_VALUES, onSubmitSuccess, onCancel }) {
+function ReviewForm({ initialValues = INITIAL_VALUES, initialPreview, onSubmitSuccess, onCancel }) {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [submittingError, setSubmittingError] = useState(null);
   const [values, setValues] = useState(initialValues);
@@ -50,7 +50,7 @@ function ReviewForm({ initialValues = INITIAL_VALUES, onSubmitSuccess, onCancel 
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
-      <FileInput name="imgFile" value={values.imgFile} onChange={handleChange} />
+      <FileInput name="imgFile" value={values.imgFile} initialPreview={initialPreview} onChange={handleChange} />
       <input name="title" value={values.title} onChange={handleInputChange} />
       <RatingInput name="rating" value={values.rating} onChange={handleChange} />
       <textarea name="content" value={values.content} onChange={handleInputChange} />
