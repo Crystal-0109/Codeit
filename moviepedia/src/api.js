44d2ffa050b4d62714a71e5c747180d093966b5d
soojung moon <parkfam044@naver.com>
2024-02-13 18:00:43 +0900

    리퀘스트 보낼 주소 다시 원래대로 수정

--- moviepedia/src/api.js
+++ moviepedia/src/api.js
@@ -1,6 +1,6 @@
 export async function getReviews({ order = "createdAt", offset = 0, limit = 6 }) {
   const query = `order=${order}&offset=${offset}&limit=${limit}`;
-  const response = await fetch(`https://learn.codeit.kr/error/film-reviews?${query}`);
+  const response = await fetch(`https://learn.codeit.kr/api/film-reviews?${query}`);
   if (!response.ok) {
     throw new Error("리뷰를 불러오는 데 실패했습니다.");
   }
