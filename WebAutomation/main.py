dfa050c2dffd735cdab3c9fbe5169595262b0d74
soojung moon <parkfam044@naver.com>
2024-02-14 16:59:52 +0900

    BeautifulSoup 설치 후 html 출력

--- WebAutomation/main.py
+++ WebAutomation/main.py
@@ -1,13 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
-rating_pages = []
-# https://https://workey.codeit.kr/ratings/index?year=2010&month=1&weekIndex=0
 
-for i in range(5):
-    url = "https://workey.codeit.kr/ratings/index?year=2010&month=1&weekIndex={}".format(i)
-    response = requests.get(url)
-    rating_page = response.text
-    rating_pages.append(rating_page)
-    
-print(len(rating_pages))
-print(rating_pages[0])
+response = requests.get("https://workey.codeit.kr/ratings/index")
+rating_page = response.text
+
+soup = BeautifulSoup(rating_page, 'html.parser')
+
+print(soup.prettify())
