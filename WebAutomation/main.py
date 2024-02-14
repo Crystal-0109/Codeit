5673d4ff383494f1088cfeaf02711f8041f753dd
soojung moon <parkfam044@naver.com>
2024-02-14 17:05:36 +0900

    제일 위에 나타나는 태그만 출력

--- WebAutomation/main.py
+++ WebAutomation/main.py
@@ -9,11 +9,11 @@ soup = BeautifulSoup(rating_page, 'html.parser')
 
 program_title_tags = soup.select('td.program')
 
-program_titles = []
+# program_titles = []
 
-for tag in program_title_tags:
-    program_titles.append(tag.get_text())
+# for tag in program_title_tags:
+#     program_titles.append(tag.get_text())
 
-print(program_titles)
+# print(program_titles)
 
-# print(soup.select_one('td.program'))
+print(soup.select_one('td.program'))
