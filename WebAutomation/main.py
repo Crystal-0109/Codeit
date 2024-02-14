5e7cf359ee09c8e3b1ed6c2c1eab5f6c4589e7a4
soojung moon <parkfam044@naver.com>
2024-02-14 16:34:21 +0900

    여러 웹사이트 한꺼번에 가져오기

--- WebAutomation/main.py
+++ WebAutomation/main.py
@@ -1,4 +1,13 @@
 import requests
 
-response = requests.get("https://google.com")
-print(response)
+rating_pages = []
+# https://https://workey.codeit.kr/ratings/index?year=2010&month=1&weekIndex=0
+
+for i in range(5):
+    url = "https://workey.codeit.kr/ratings/index?year=2010&month=1&weekIndex={}".format(i)
+    response = requests.get(url)
+    rating_page = response.text
+    rating_pages.append(rating_page)
+    
+print(len(rating_pages))
+print(rating_pages[0])
