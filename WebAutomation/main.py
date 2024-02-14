2f47fbbfe8d8421abd8838790f927aed787ec871
soojung moon <parkfam044@naver.com>
2024-02-14 18:07:48 +0900

    태그에서 속성 빼오기

--- WebAutomation/main.py
+++ WebAutomation/main.py
@@ -6,12 +6,4 @@ music_page = response.text
 
 soup = BeautifulSoup(music_page, 'html.parser')
 
-popular_artists = []
-
-# for tag in soup.select('ul.popular__order li'):
-#     popular_artists.append(tag.get_text().strip())
-
-for tag in soup.select('ul.popular__order li'):
-    popular_artists.append(list(tag.stripped_strings)[1])
-    
-print(popular_artists)    
+print(soup.select_one('img').attrs)
