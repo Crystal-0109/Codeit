93969426c86ec4cda86c59e846b162df21f92dff
soojung moon <parkfam044@naver.com>
2024-02-14 18:46:58 +0900

    데이터를 csv 파일로 저장

--- WebAutomation/main.py
+++ WebAutomation/main.py
@@ -1,10 +1,13 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
-from openpyxl import Workbook
 
-wb = Workbook(write_only=True)
-ws = wb.create_sheet('TV ratings')
-ws.append(['순위', '채널', '프로그램', '시청률'])
+# CSV 파일 생성
+csv_file = open('시청률_2010년1월1주차.csv', 'w')
+csv_writer = csv.writer(csv_file)
+
+# 헤더 행 추가
+csv_writer.writerow(['순위', '채널', '프로그램', '시청률'])
 
 response = requests.get("https://workey.codeit.kr/ratings/index")
 rating_page = response.text
@@ -14,11 +17,13 @@ soup = BeautifulSoup(rating_page, 'html.parser')
 for tr_tag in soup.select('tr')[1:]:
     td_tags = tr_tag.select('td')
     row = [
-        td_tags[0].get_text(), # 순위
-        td_tags[1].get_text(), # 채널
-        td_tags[2].get_text(), # 프로그램
-        td_tags[3].get_text(), # 시청률
+        td_tags[0].get_text(),
+        td_tags[1].get_text(),
+        td_tags[2].get_text(),
+        td_tags[3].get_text()
     ]
-    ws.append(row)
-    
-wb.save('시청률_2010년1월1주차.xlsx')
+    # 데이터 행 추가
+    csv_writer.writerow(row)
+
+# CSV 파일 닫기
+csv_file.close()
