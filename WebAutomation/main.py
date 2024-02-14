97f63aa5e6131f7233743c216302b511a276f438
soojung moon <parkfam044@naver.com>
2024-02-14 17:04:31 +0900

    TV 제목을 리스트에 담아서 출력

--- WebAutomation/main.py
+++ WebAutomation/main.py
@@ -7,4 +7,13 @@ rating_page = response.text
 
 soup = BeautifulSoup(rating_page, 'html.parser')
 
-print(soup.select('table'))
+program_title_tags = soup.select('td.program')
+
+program_titles = []
+
+for tag in program_title_tags:
+    program_titles.append(tag.get_text())
+
+print(program_titles)
+
+# print(soup.select_one('td.program'))
