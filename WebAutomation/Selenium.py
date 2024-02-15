e8377517c5ad0c691bd9e1d22457a0893bce754c
soojung moon <parkfam044@naver.com>
2024-02-15 18:36:50 +0900

    웹페이지 끝까지 스크롤하기

--- WebAutomation/Selenium.py
+++ WebAutomation/Selenium.py
@@ -13,7 +13,18 @@ driver.implicitly_wait(3)
 driver.get('https://workey.codeit.kr/music')
 time.sleep(1)
 
-driver.execute_script("window.scrollTo(0, 200);")
+# 현재 scrollHeight 가져오기
+last_height = driver.execute_script("return document.body.scrollHeight;")
 
-scroll_height = driver.execute_script("return document.body.scrollHeight;")
-print(scroll_height)
+while True:
+    # scrollHeight까지 스크롤
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # 새로운 내용 로딩될때까지 기다림
+    time.sleep(0.5)
+
+    # 새로운 내용 로딩됐는지 확인
+    new_height = driver.execute_script("return document.body.scrollHeight;")
+    if new_height == last_height:
+        break
+    last_height = new_height
