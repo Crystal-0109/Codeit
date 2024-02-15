3d00b99d004d89c4c0f9ce25a4f7e0ad783704d5
soojung moon <parkfam044@naver.com>
2024-02-15 18:27:07 +0900

    셀레니움으로 웹 스크래핑(인기 아티스트 순위 출력)

--- WebAutomation/Selenium.py
+++ WebAutomation/Selenium.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
 import time
 
 # 브라우저 꺼짐 방지 옵션
@@ -9,17 +8,14 @@ chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 
-driver = webdriver.Chrome() 
 driver.implicitly_wait(3)
 
-driver.get('https://workey.codeit.kr/costagram/index')
+driver.get('https://workey.codeit.kr/music')
 time.sleep(1)
 
-driver.find_element(By.CSS_SELECTOR, '.top-nav__login-link').click()
-time.sleep(1)
+popular_artists =[]
 
-driver.find_element(By.CSS_SELECTOR, '.login-container__login-input').send_keys('codeit')
-driver.find_element(By.CSS_SELECTOR, '.login-container__password-input').send_keys('datascience')
-driver.find_element(By.CSS_SELECTOR, '.login-container__login-button').click()
+for artist in driver.find_elements(By.CSS_SELECTOR, 'ul.popular__order li'):
+  popular_artists.append(artist.text.strip())
 
-time.sleep(10)
+print(popular_artists)
