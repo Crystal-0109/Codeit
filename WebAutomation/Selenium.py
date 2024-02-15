da66b3feb5275356a50f778582d0fe8fe0bfd244
soojung moon <parkfam044@naver.com>
2024-02-15 18:11:35 +0900

    로딩될 때까지 잠깐 기다리기

--- WebAutomation/Selenium.py
+++ WebAutomation/Selenium.py
@@ -11,9 +11,13 @@ driver = webdriver.Chrome(options=chrome_options)
 
 driver = webdriver.Chrome() 
 driver.implicitly_wait(3)
+
 driver.get('https://workey.codeit.kr/costagram/index')
+time.sleep(1)
 
 driver.find_element(By.CSS_SELECTOR, '.top-nav__login-link').click()
+time.sleep(1)
+
 driver.find_element(By.CSS_SELECTOR, '.login-container__login-input').send_keys('codeit')
 driver.find_element(By.CSS_SELECTOR, '.login-container__password-input').send_keys('datascience')
 driver.find_element(By.CSS_SELECTOR, '.login-container__login-button').click()
