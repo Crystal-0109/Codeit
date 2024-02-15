53b6aeda9bb89374318b512479025c6c34005cbe
soojung moon <parkfam044@naver.com>
2024-02-15 18:07:50 +0900

    웹사이트 제어(자동 로그인)

--- WebAutomation/Selenium.py
+++ WebAutomation/Selenium.py
@@ -1,10 +1,21 @@
-# Selenium 임포트
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
 import time
 
-# 크롬 드라이버 생성
-driver = webdriver.Chrome()
+# 브라우저 꺼짐 방지 옵션
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
 
-# 사이트 접속하기
-driver.get('https://codeit.kr')
-# time.sleep(100)
+driver = webdriver.Chrome() 
+driver.implicitly_wait(3)
+driver.get('https://workey.codeit.kr/costagram/index')
+
+driver.find_element(By.CSS_SELECTOR, '.top-nav__login-link').click()
+driver.find_element(By.CSS_SELECTOR, '.login-container__login-input').send_keys('codeit')
+driver.find_element(By.CSS_SELECTOR, '.login-container__password-input').send_keys('datascience')
+driver.find_element(By.CSS_SELECTOR, '.login-container__login-button').click()
+
+time.sleep(10)
