4cafb83d35378602707f53fbc22dd27bfee90c3f
soojung moon <parkfam044@naver.com>
2024-02-21 21:07:57 +0900

    그루핑해서 보기 1 (group by)

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -1,16 +1,5 @@
 USE copang_main;
 
-select *, length(address)
-from member;
-
-select email, upper(email)
-from member;
-
-select email, lower(email)
-from member;
-
-select email, lpad(age, 10, '0')
-from member;
-
-select email, rpad(age, 10, '!')
-from member;
+select gender, count(*), avg(height), min(weight)
+from member
+group by gender;
