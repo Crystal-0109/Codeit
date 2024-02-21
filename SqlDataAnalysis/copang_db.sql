4adfb6b67c8f381f767fdb587d8431eb4c84c0ff
soojung moon <parkfam044@naver.com>
2024-02-21 21:02:36 +0900

    고유값만 보기 (distinct, substring)

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -1,13 +1,10 @@
 USE copang_main;
 
-select email, concat(height, 'cm', ', ', weight, 'kg') as '키와 몸무게', weight / ((height / 100) * (height / 100)) as BMI,
-(case
-	when weight is null or height is null then '비만 여부 알 수 없음'
-    when weight / ((height / 100) * (height / 100)) >= 25 then '과체중 또는 비만'
-    when weight / ((height / 100) * (height / 100)) >= 18.5
-		 and weight / ((height / 100) * (height / 100)) < 25
-         then '정상'
-	else '저체중'
-end) as obesity_check
-from member
-order by obesity_check;
+select distinct(gender)
+from member;
+
+select distinct(address)
+from member;
+
+select distinct(substring(address, 1, 2))
+from member;
