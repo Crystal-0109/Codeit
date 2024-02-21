f6ba1899633b11b26b15243b1d54772e49ef3620
soojung moon <parkfam044@naver.com>
2024-02-21 20:15:58 +0900

    데이터 일부만 추려보기 (limit)

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -2,17 +2,10 @@ USE copang_main;
 
 select *
 from member
-order by height asc;
+order by sign_up_day desc
+limit 10;
 
 select *
 from member
-order by height desc;
-
-select *
-from member
-where gender = 'm' and weight >= 70
-order by height asc;
-
-select sign_up_day, email
-from member
-order by year(sign_up_day) desc, email asc;
+order by sign_up_day desc
+limit 8, 2;
