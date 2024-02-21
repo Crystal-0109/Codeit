a6fd76440132b2c6dd187d7482b507fc17d7b1e7
soojung moon <parkfam044@naver.com>
2024-02-21 23:38:11 +0900

    그루핑해서 보기 4(심화) (with rollup)

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -1,22 +1,8 @@
 USE copang_main;
 
-select substring(address, 1, 2) as region, count(*)
-from member
-group by substring(address, 1, 2);
-
 select substring(address, 1, 2) as region, gender, count(*)
 from member
 group by substring(address, 1, 2), gender
-having region = '서울'
-and gender = 'm';
-
-select substring(address, 1, 2) as region, gender, count(*)
-from member
-group by substring(address, 1, 2), gender
-having region is not null;
-
-select substring(address, 1, 2) as region, gender, count(*)
-from member
-group by substring(address, 1, 2), gender
+with rollup
 having region is not null
 order by region asc, gender desc;
