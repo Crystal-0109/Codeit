a17c8cb0a99c3ac0bdf8d87b6dee6ea4ad580932
soojung moon <parkfam044@naver.com>
2024-02-21 20:21:01 +0900

    데이터의 특성 구하기 (count, min, max, avg)

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -1,11 +1,19 @@
 USE copang_main;
 
-select *
-from member
-order by sign_up_day desc
-limit 10;
+select count(email)
+from member;
 
-select *
-from member
-order by sign_up_day desc
-limit 8, 2;
+select count(height)
+from member;
+
+select count(*)
+from member;
+
+select max(height)
+from member;
+
+select min(weight)
+from member;
+
+select avg(weight)
+from member;
