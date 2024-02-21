46d5c9d1958a0c3cb6feba8d5d5dc7c436e4c03b
soojung moon <parkfam044@naver.com>
2024-02-21 19:00:07 +0900

    문자열 패턴 매칭 조건

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -2,20 +2,8 @@ USE copang_main;
 
 select *
 from member
-where age >= 27;
+where address like '서울%';
 
 select *
 from member
-where age between 30 and 39;
-
-select *
-from member
-where age not between 30 and 39;
-
-select *
-from member
-where sign_up_day > '2019-01-01';
-
-select *
-from member
-where sign_up_day between '2018-01-01' and '2018-12-31';
+where address like '%고양시%';
