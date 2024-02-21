b1e2535d32ae5569b60cdaf63c63591961b20e5e
soojung moon <parkfam044@naver.com>
2024-02-21 23:17:47 +0900

    그루핑해서 보기 3 (having)

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -6,4 +6,17 @@ group by substring(address, 1, 2);
 
 select substring(address, 1, 2) as region, gender, count(*)
 from member
-group by substring(address, 1, 2), gender;
+group by substring(address, 1, 2), gender
+having region = '서울'
+and gender = 'm';
+
+select substring(address, 1, 2) as region, gender, count(*)
+from member
+group by substring(address, 1, 2), gender
+having region is not null;
+
+select substring(address, 1, 2) as region, gender, count(*)
+from member
+group by substring(address, 1, 2), gender
+having region is not null
+order by region asc, gender desc;
