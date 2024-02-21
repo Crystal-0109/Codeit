a3c6c6ff33da40df23645c82491b6ea9e7fa8732
soojung moon <parkfam044@naver.com>
2024-02-21 18:49:12 +0900

    조건을 나타내는 다양한 방법(where)

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -1,10 +1,21 @@
-CREATE DATABASE copang_main;
 USE copang_main;
 
-select * from member;
-
-select email, age, address from member;
-
-select * 
+select *
 from member
-where email = 'taehos@hanmail.net';
+where age >= 27;
+
+select *
+from member
+where age between 30 and 39;
+
+select *
+from member
+where age not between 30 and 39;
+
+select *
+from member
+where sign_up_day > '2019-01-01';
+
+select *
+from member
+where sign_up_day between '2018-01-01' and '2018-12-31';
