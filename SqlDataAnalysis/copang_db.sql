faa57175ce46e6675fe206a372a1b247e4f31ac0
soojung moon <parkfam044@naver.com>
2024-02-22 15:26:35 +0900

    뷰 생성, 조회

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -1,18 +1,19 @@
 use copang_main;
 
-# 서브쿼리 중첩
-select i.id, i.name, avg(star) avg_star, count(*) count_star
+# 뷰 생성
+create view three_tables_joined as
+select i.id, i.name, avg(star) as avg_star, count(*) as count_star
 from item as i left outer join review as r on r.item_id = i.id
-     left outer join member as m on r.mem_id = m.id
+	 left outer join member as m on r.mem_id = m.id
 where m.gender = 'f'
 group by i.id, i.name
 having count(*) >= 2
-	   and avg_star = (select max(avg_star)
-					   from (select i.id, i.name, avg(star) as avg_star, count(*) as count_star
-							 from item as i left outer join review as r on r.item_id = i.id
-								  left outer join member as m on r.mem_id = m.id
-							 where m.gender = 'f'
-                             group by i.id, i.name
-                             having count(*) >= 2
-                             order by avg(star) desc, count(*) desc) as final)
 order by avg(star) desc, count(*) desc;
+
+# 뷰 사용해서 별점의 평균값이 가장 높은 상품 조회
+SELECT * 
+FROM three_tables_joined
+where avg_star = (select max(avg_star)
+				  from three_tables_joined)
+	  and count_star = (select max(count_star)
+						from three_tables_joined);
