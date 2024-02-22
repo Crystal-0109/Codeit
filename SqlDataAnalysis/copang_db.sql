9315b9185dd69d835a8eb87a75b79ceea341fdaa
soojung moon <parkfam044@naver.com>
2024-02-22 14:10:07 +0900

    where 절에 있는 서브쿼리 2

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -1,17 +1,9 @@
 use copang_main;
 
-# 전체 상품의 평균 가격보다 높은 가격을 가진 상품 조회
-select id, name, price,
-	   (select avg(price) from item) as 'avg_price'
+# 리뷰가 최소 3개 이상 달린 상품들의 정보 조회
+select *
 from item
-where price > (select avg(price) from item);
-
-# 가장 비싼 상품 조회
-select id, name, price
-from item
-where price = (select max(price) from item);
-
-# 최저가 상품 조회
-select id, name, price
-from item
-where price = (select min(price) from item);
+where id in (select item_id
+			 from review
+             group by item_id
+             having count(*) >= 3);
