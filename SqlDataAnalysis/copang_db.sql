e8ab93581b24a4eb79cc3634dd89184ef74a75f6
soojung moon <parkfam044@naver.com>
2024-02-22 12:15:10 +0900

    내부 조인

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -1,15 +1,8 @@
 use copang_main;
 
-select item.id,
-	   item.name,
-       stock.item_id,
-       stock.inventory_count
-from item left outer join stock
-on item.id = stock.item_id;
-
 select i.id,
 	   i.name,
        s.item_id,
        s.inventory_count
-from item as i right outer join stock s
+from item as i inner join stock s
 on i.id = s.item_id;
