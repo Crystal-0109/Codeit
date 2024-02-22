08ed1b8049d969cba67ffe87e646dda554962c00
soojung moon <parkfam044@naver.com>
2024-02-22 13:17:16 +0900

    같은 종류의 테이블 조인하기 (union)

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -1,8 +1,26 @@
 use copang_main;
 
-select i.id,
-	   i.name,
-       s.item_id,
-       s.inventory_count
-from item as i inner join stock s
-on i.id = s.item_id;
+select old.id as old_id,
+	   old.name as old_name,
+       new.id as new_id,
+       new.name as new_name
+from item as old left outer join item_new as new
+on old.id = new.id;
+
+select old.id as old_id,
+	   old.name as old_name,
+       new.id as new_id,
+       new.name as new_name
+from item as old right outer join item_new as new
+on old.id = new.id;
+
+select old.id as old_id,
+	   old.name as old_name,
+       new.id as new_id,
+       new.name as new_name
+from item as old inner join item_new as new
+on old.id = new.id;
+
+select * from item
+union
+select * from item_new;
