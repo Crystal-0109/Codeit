03d9b69dd806c2e693a850f9b91234d104c3b61e
soojung moon <parkfam044@naver.com>
2024-02-22 13:34:48 +0900

    의미있는 데이터 추출하기 2

--- SqlDataAnalysis/copang_db.sql
+++ SqlDataAnalysis/copang_db.sql
@@ -1,8 +1,21 @@
 use copang_main;
 
-select i.id, i.name, avg(star)
+select i.id, i.name, avg(star), count(*)
 from item as i left outer join review as r on r.item_id = i.id
      left outer join member as m on r.mem_id = m.id
 where m.gender = 'f'
 group by i.id, i.name
-order by avg(star) desc;
+having count(*) > 1
+order by avg(star) desc, count(*) desc;
+
+select i.id, i.name, avg(star), count(*)
+from item as i left outer join review as r on r.item_id = i.id
+     left outer join member as m on r.mem_id = m.id
+where m.gender = 'm'
+group by i.id, i.name
+having count(*) > 1
+order by avg(star) desc, count(*) desc;
+
+select *
+from review
+where item_id = 2;
