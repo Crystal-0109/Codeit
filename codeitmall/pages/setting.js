647daf23816b0d73694ad630629e2f9cbe1fb95d
soojung moon <parkfam044@naver.com>
2024-02-24 18:20:27 +0900

    Context 활용하기

--- codeitmall/pages/setting.js
+++ codeitmall/pages/setting.js
@@ -1,14 +1,28 @@
 import Container from "@/components/Container";
 import Header from "@/components/Header";
+import { useTheme } from "@/lib/ThemeContext";
 import styles from "@/styles/Setting.module.css";
+import Dropdown from "@/components/Dropdown";
 
 export default function Setting() {
+  const { theme, setTheme } = useTheme();
+
   return (
-    <>
+    <div>
       <h1 className={styles.title}>설정</h1>
       <section className={styles.section}>
         <h2 className={styles.sectionTitle}>테마 설정</h2>
+        <Dropdown
+          className={styles.input}
+          name="theme"
+          value={theme}
+          onChange={(name, value) => setTheme(value)}
+          options={[
+            { label: "라이트", value: "light" },
+            { label: "다크", value: "dark" },
+          ]}
+        />
       </section>
-    </>
+    </div>
   );
 }
